03fc84cde8376fd1148f2afcd3b86ecbd4b5a329
vincent porte <vincent@neuralia.co>
2025-12-09 16:44:38 +0100

    add comment and reformat ollama_quick_start

--- mcp_clients/ollama_quick_start.py
+++ mcp_clients/ollama_quick_start.py
@@ -1,10 +1,19 @@
 from ollama import ChatResponse, chat
 
 
-response: ChatResponse = chat(model='gemma3:4b', messages=[
-    {
-    'role': 'user',
-    'content': 'Why is the sky blue?',
-  },
-])
+"""
+Very simple script, calling llama model with a fixed prompt.
+Then, it prints the answer.
+For demo only.
+"""
+
+response: ChatResponse = chat(
+    model="gemma3:4b",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is the sky blue?",
+        },
+    ],
+)
 print(response.message.content)
